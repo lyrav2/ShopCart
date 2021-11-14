296104871ff5bbd3d36fb0e57c2a805ae7891159
lyrav2 <lyrav2@gmail.com>
2021-11-14 18:04:23 -0500

    added netlify app to cors

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ if (process.env.NODE_ENV!="production") {
 const app = express();
 
 const corsOptionsDelegate = function(req, callback) {
-    const allowlist = ['http://localhost:3000', 'http://127.0.0.1:3000']
+    const allowlist = ['http://localhost:3000', 'http://127.0.0.1:3000', 'https://relaxed-ritchie-40df0a.netlify.app']
     let corsOptions;
     if (allowlist.indexOf(req.header('Origin')) !== -1) {
         corsOptions = {origin: true}
